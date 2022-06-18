8e5084f912c41d77a73a5448a7886e3c885edeba
Erutuf-Roy <erutuf.blueshiner@gmail.com>
2022-06-18 20:37:07 +0800

    Upload files

--- main.py
+++ main.py
@@ -21,9 +21,66 @@ def output(path, data):
     return
 
 
+import numpy as np
+import pandas as pd
+from scipy.stats import skew
+from scipy.stats import kurtosis
+
 if __name__ == "__main__":
     args = config()
 
-    data = [["2018-01-01 00:00:00", "buy", 2.5, 3],
-            ["2018-01-01 01:00:00", "sell", 3, 5]]
-    output(args.output, data)
+    import joblib
+    clf = joblib.load("gbm.pkl")
+
+    dd1 = pd.read_csv(args.consumption, index_col=None)
+    dd2 = pd.read_csv(args.generation, index_col=None)
+
+    dd1['generation'] = dd2['generation'].copy()
+    dd1['time'] = pd.to_datetime(dd1['time'])
+    dd1.set_index('time', inplace=True)
+
+    df_all = []
+    for i in range( 24 ):
+        df_all.append( dd1.loc[dd1.index.to_series().dt.hour == i] )
+
+    dataset_x = []
+    for t in range( 24 ):
+        tmp_x = []
+        atmp = df_all[t].reset_index().drop('time', axis=1).iloc[:]
+        aatmp = atmp.groupby(atmp.index // 7 ).agg({'min', 'max', 'sum', skew, kurtosis}).values
+        tmp_x.append(aatmp)
+
+        dataset_x.append(tmp_x)
+
+    for i in range( 24 ):
+        dataset_x[i] = np.asarray(dataset_x[i])
+
+        
+    cmd = []
+    for t in range(24):
+        one_col = []
+        pred = clf[t].predict(dataset_x[t][0])[0]
+
+        # Time
+        one_col.append(str(dd1.index[-1] + pd.DateOffset(hours=t+1)))
+        # buy or sell
+        if pred >= 0:
+            one_col.append("buy")
+            # prices
+            one_col.append(2.51)
+            # volume
+            one_col.append(round(pred, 2))
+        else:
+            one_col.append("sell")
+            # prices
+            one_col.append(0.01)
+            # volumn
+            one_col.append(round(pred * -1, 2))
+
+        print(one_col)
+        cmd.append(one_col)
+
+    print(cmd)
+    #output(args.output, data)
+
+    output(args.output, cmd)
