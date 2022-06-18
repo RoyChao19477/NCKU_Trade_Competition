07b3ed7a3f191ece3287941ffc2fc8a3bcf31e3c
Erutuf-Roy <erutuf.blueshiner@gmail.com>
2022-06-18 21:15:57 +0800

    Update readme

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ import pandas as pd
 from scipy.stats import skew
 from scipy.stats import kurtosis
 
+import random
+
 if __name__ == "__main__":
     args = config()
 
@@ -59,26 +61,125 @@ if __name__ == "__main__":
     cmd = []
     for t in range(24):
         one_col = []
+        h_vol = 0
+        l_vol = 0
         pred = clf[t].predict(dataset_x[t][0])[0]
+        # Time
+        one_col.append(str(dd1.index[-1] + pd.DateOffset(hours=t+1)))
+        # buy or sell
+        haha_price = 0
+        haha_value = 0
 
+        if pred >= 0:
+            one_col.append("buy")
+
+            # one third
+            h_vol = round(pred / random.randint(3, 6), 2)
+            l_vol = round(pred - h_vol, 2)
+
+            # prices
+            if ((t+1) <= 6) or ((t+1) >= 18):
+                haha_price = round(random.randint(253,350) / 100, 2)
+                haha_value = h_vol
+                one_col.append(round(haha_price, 2))
+
+            else:
+                one_col.append(round(random.randint(255,260) / 100, 2))
+            # volume
+            #one_col.append(round(pred, 2))
+            one_col.append(round(h_vol, 2))
+            
+        else:
+            one_col.append("sell")
+            # prices
+            # one_col.append(0.01)
+            h_vol = round((pred * -1) / random.randint(6, 12), 2)
+            l_vol = round((pred * -1) - h_vol, 2)
+            
+            one_col.append(0.01)
+            #one_col.append(random.randint(180,240) / 100)
+            # volumn
+            one_col.append(round(h_vol, 2))
+
+        print(one_col)
+        cmd.append(one_col)
+
+        #---------------------------------------------------
+
+        one_col = []
+        one_col.append(str(dd1.index[-1] + pd.DateOffset(hours=t+1)))
+        # buy or sell
+        if pred >= 0:
+            one_col.append("buy")
+
+            # one third
+            #h_vol = round(pred / random.randint(3, 6), 2)
+            #l_vol = round(pred - h_vol, 2)
+
+            # prices
+            one_col.append(2.52)
+            # volume
+            #one_col.append(round(pred, 2))
+            one_col.append(round(l_vol, 2))
+            print(one_col)
+            cmd.append(one_col)
+        else:
+            one_col.append("sell")
+            # prices
+            # one_col.append(0.01)
+            #pred = pred * -1
+            #h_vol = round(pred / random.randint(3, 6), 2)
+            #l_vol = round(pred - h_vol, 2)
+            
+            #one_col.append(0.01)
+            one_col.append(random.randint(240,253) / 100)
+            # volumn
+            one_col.append(round(l_vol, 2))
+            print(one_col)
+            cmd.append(one_col)
+
+        #---------------------------------------------------
+
+        one_col = []
         # Time
         one_col.append(str(dd1.index[-1] + pd.DateOffset(hours=t+1)))
         # buy or sell
         if pred >= 0:
             one_col.append("buy")
-            # prices
-            one_col.append(2.51)
-            # volume
-            one_col.append(round(pred, 2))
+            one_col.append(0.01)
+            one_col.append(random.randint(1,2) / 100)
         else:
             one_col.append("sell")
-            # prices
-            one_col.append(0.01)
-            # volumn
-            one_col.append(round(pred * -1, 2))
-
+            one_col.append(round(random.randint(254,270) / 100, 2))
+            one_col.append(random.randint(300, 1000) / 100)
+        
         print(one_col)
         cmd.append(one_col)
+
+        #---------------------------------------------------
+
+        one_col = []
+        # Time
+        one_col.append(str(dd1.index[-1] + pd.DateOffset(hours=t+1)))
+        # buy or sell
+        if pred >= 0:
+            if ((t+1) <= 6) or ((t+1) >= 18):
+                one_col.append("sell")
+                one_col.append(0.01)
+                one_col.append(round(haha_value, 2))
+            
+            else:
+                one_col.append("sell")
+                one_col.append(round(random.randint(254,270) / 100, 2))
+                one_col.append(random.randint(1000,2000) / 100)
+        else:
+            one_col.append("sell")
+            one_col.append(2.53)
+            one_col.append(random.randint(300, 1000) / 100)
+        
+        print(one_col)
+        cmd.append(one_col)
+
 
     print(cmd)
     #output(args.output, data)
